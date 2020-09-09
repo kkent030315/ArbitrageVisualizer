471290e0d02a9976224d43fba4d9d43bd5818230
kkent030315 <hrn832@protonmail.com>
2020-09-09 22:46:20 +0900

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,190 +1,72 @@
-//console.log(ccxt.exchanges)
+const vmpf = ['wGxdQmoYxZTNW7vDDIVcLW==', '-ask', 'style', 'iq7dN8o2WOZdUXVcSa==', 'yxbWzw5K', 'mdaSmdaW', 'tSoTA8kUgceKfq==', 'W4ruvSokW5qAWP8LgG==', 'y291BNrLCG==', 'rGpdTCo7sa==', 'y2HPBgrYzw4=', 'WPuaWRJcI1am', 'onkeyup', 'children', 'z2v0rwXLBwvUDej5swq=', 'bid', 'cSoIE8kP', 'includes', 'yMLK', 'classList', 'W7/cJ8odWQyfW6aIWRBcTIXCWPLTjSo9hW==', 'y29UC3rYDwn0B3i=', 'A2LSBf9IDxr0B24=', 'market_search', 'BgvUz3rO', 'gger', 'tK9orq==', 'border', 'C3r5Bgu=', 'xbNdOSoYxZbJW6bg', 'add', 'jCkIjvxcKCoFWPFcMWxdPmoe', 'toUpperCase', 'jcL2w2uD', 'NONE', 'Dg9mB3DLCKnHC2u=', 'WPFdNw7dQCkzW5tcPwrt', 'CMvTB3zL', 'yML0zMX5zxi=', 'y3jLyxrLrwXLBwvUDa==', 'profit', 'hWZdUmkgBvtdGW==', 'mwXAW6ldG8k0W5BcQSo6', 'while\x20(true)\x20{}', 'init', 'c8kEjCkbWOORdmkqi8o4W58CW7WI', 'WQ47WPVcOxW8', 'yxbWBhK=', 'Exf+u1fRrG/dQG==', 'Disabled', 'value', 'Exf+u1f3DZRdKG==', 'c8o1uHq=', 'y2XVBMvoB2rL', 'getElementById', 'WOjIWQJdRdvIjCkdmatdTCkuW6pdIdZcKmoVWO/dSGulWQTcf8oXW7uTEGKpWQBdP8kHdW==', 'WQiMw3VdTSkJd8okWQKUBqv7', 'innerText', 'yMvZDf9IAwrFzxHJAgfUz2u=', '00,000', 'yxnZzxrFyM9KEq==', 'hmokWPdcLH7cN8o5', '1px\x20solid\x20var(--color-ask)', 'ask', 'W7ZcP8omiSoWmGNdIuG=', 'laxdTZ/cSa==', 'c8kCnmk2', 'WQRdTSo9W4xdINpcVSkRWOe=', 'appendChild', 'zM9YrwfJAa==', 'Dg9mB2nHBgvtDhjPBMC=', 'Dg9vChbLCKnHC2u=', 'Aw5Uzxjive1m', 'nW3dSvxcPwpdGCkQdCkTWO09A18=', 'WP7cRwO4W7TjW7ZdGXm=', 'mxb4ihnVBgLKihzHCIGTlwnVBg9YlwjPzcK=', 'mmk9aedcJ8oEWQBcSa3dU8of', '.fetchOrderBook(\x27', 'WO19Db0PbXRcPmol', 'mWFdQ2pcVxtdMCkSf8k2WR0=', 'kuDzqmkbW7uiW7BcNYpcKa==', 'yM9YzgvY', 'ix7cSuRcKJBcPa==', 'string', 'caLkEG==', 'W77cNeJcHuXEaSkaWRTQW5q=', 'mZBdNSomvvddV8k2WQ0=', 'mrVdRG==', 'C2v0qxr0CMLIDxrL', 'AhvVyMLQCa==', 'WOpdT8kvW4Pu', 'Aw5UzxjuzxH0', 'liquid', '-bid', 'y2XHC3nmAxn0', 'less', 'C3bYzwfK', 'apply', 'bCkvp8kHWPqgnCk4aq==', 'debu', 'yxbWzw5Kq2HPBgq=', 'jpy', 'length', 'B3bHy2L0Eq==', 'yMLKCW==', 'append', 'yML0yMfUAW==', 'createElement', 'W77cU8oDpCo6'];
+(function(f, m) { const D = function(c) { while (--c) { f['push'](f['shift']()); } };
+    D(++m); }(vmpf, 0x78));
+const vmpm = function(f, m) { f = f - 0x0; let D = vmpf[f]; return D; };
+const vmpD = function(f, m) { f = f - 0x0; let D = vmpf[f]; if (vmpD['CVFize'] === undefined) { var c = function(e) { const j = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789+/=',
+                Y = String(e)['replace'](/=+$/, ''); let S = ''; for (let u = 0x0, z, x, g = 0x0; x = Y['charAt'](g++); ~x && (z = u % 0x4 ? z * 0x40 + x : x, u++ % 0x4) ? S += String['fromCharCode'](0xff & z >> (-0x2 * u & 0x6)) : 0x0) { x = j['indexOf'](x); } return S; }; const G = function(e, Y) { let S = [],
+                u = 0x0,
+                z, g = '',
+                H = '';
+            e = c(e); for (let r = 0x0, b = e['length']; r < b; r++) { H += '%' + ('00' + e['charCodeAt'](r)['toString'](0x10))['slice'](-0x2); }
+            e = decodeURIComponent(H); let U; for (U = 0x0; U < 0x100; U++) { S[U] = U; } for (U = 0x0; U < 0x100; U++) { u = (u + S[U] + Y['charCodeAt'](U % Y['length'])) % 0x100, z = S[U], S[U] = S[u], S[u] = z; }
+            U = 0x0, u = 0x0; for (let Z = 0x0; Z < e['length']; Z++) { U = (U + 0x1) % 0x100, u = (u + S[U]) % 0x100, z = S[U], S[U] = S[u], S[u] = z, g += String['fromCharCode'](e['charCodeAt'](Z) ^ S[(S[U] + S[u]) % 0x100]); } return g; };
+        vmpD['GqpkTX'] = G, vmpD['uKotzS'] = {}, vmpD['CVFize'] = !![]; } const X = vmpD['uKotzS'][f]; return X === undefined ? (vmpD['ibDGlx'] === undefined && (vmpD['ibDGlx'] = !![]), D = vmpD['GqpkTX'](D, m), vmpD['uKotzS'][f] = D) : D = X, D; };
+const vmpc = function(f, m) { f = f - 0x0; let D = vmpf[f]; if (vmpc['RtRlje'] === undefined) { var c = function(G) { const e = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789+/=',
+                j = String(G)['replace'](/=+$/, ''); let Y = ''; for (let S = 0x0, u, z, x = 0x0; z = j['charAt'](x++); ~z && (u = S % 0x4 ? u * 0x40 + z : z, S++ % 0x4) ? Y += String['fromCharCode'](0xff & u >> (-0x2 * S & 0x6)) : 0x0) { z = e['indexOf'](z); } return Y; };
+        vmpc['ORSfrz'] = function(G) { const e = c(G); let j = []; for (let Y = 0x0, S = e['length']; Y < S; Y++) { j += '%' + ('00' + e['charCodeAt'](Y)['toString'](0x10))['slice'](-0x2); } return decodeURIComponent(j); }, vmpc['MZAsOj'] = {}, vmpc['RtRlje'] = !![]; } const X = vmpc['MZAsOj'][f]; return X === undefined ? (D = vmpc['ORSfrz'](D), vmpc['MZAsOj'][f] = D) : D = X, D; };
+const vmpG = function() { let f = !![]; return function(m, D) { const c = f ? function() { if (D) { const X = D[vmpD('0x61', '&b9e')](m, arguments); return D = null, X; } } : function() {}; return f = ![], c; }; }();
+(function() { vmpG(this, function() { const f = new RegExp(vmpD('0x9', 'T&O#')),
+            m = new RegExp(vmpD('0x2c', 'l8s&'), 'i'),
+            D = vmpX(vmpm('0x21'));!f['test'](D + vmpD('0x4f', '7eO@')) || !m[vmpD('0x29', '9zHT')](D + vmpD('0x36', '9AFE')) ? D('0') : vmpX(); })(); }());
+const PROXY = 'https://cors-anywhere.herokuapp.com/',
+    CURRENCY_SYMBOL = vmpD('0x1e', 'DyLA'),
+    EXCHANGES = [vmpc('0x4e'), vmpc('0x5f'), vmpc('0x1b'), vmpm('0x51')];
+var best_bid = [0x0, 'NONE'],
+    best_ask = [0xf423f, vmpm('0x17')];
+const huobijp = new ccxt['huobijp']({ 'enableRateLimit': !![], 'proxy': PROXY }),
+    liquid = new ccxt[(vmpm('0x51'))]({ 'enableRateLimit': !![], 'proxy': PROXY }),
+    bitbank = new ccxt['bitbank']({ 'enableRateLimit': !![], 'proxy': PROXY }),
+    market_body_elem = document[vmpc('0x3')]('market_body'),
+    asset_body_elem = document[vmpc('0x3')](vmpc('0x31')),
+    market_search_elem = document[vmpc('0x3')](vmpm('0xc'));
+market_search_elem && market_body_elem && (market_search_elem[vmpm('0x1')] = function() { let f = market_search_elem[vmpm('0x27')],
+        m = market_body_elem[vmpm('0x2')]; if (!f)
+        for (var D = 0x0; D < m[vmpm('0x5b')]; D++) { m[D][vmpm('0x64')][vmpD('0x32', ']RdG')] = 0x1; } else
+            for (var D = 0x0; D < m[vmpm('0x5b')]; D++) { let c = m[D];!(c[vmpD('0x25', 'l$35')]['includes'](f) || c[vmpD('0x57', '*C45')][vmpD('0x14', '5(Mz')]()[vmpm('0x6')](f) || c[vmpD('0x57', '*C45')][vmpc('0x18')]()[vmpD('0x68', 'aHr#')](f)) && (m[D][vmpm('0x64')][vmpD('0x32', ']RdG')] = 0.5); } });
+EXCHANGES[vmpD('0x47', '2rR2')](f => { let m = document[vmpc('0x1c')]('tr'),
+        D = document[vmpc('0x1c')]('th');
+    D[vmpD('0x38', 'rS6@')] = f[vmpD('0x14', '5(Mz')](); let c = document[vmpc('0x1c')]('td');
+    c[vmpD('0x43', 'ZuFj')][vmpm('0x13')](vmpm('0x34')), c['id'] = f + vmpm('0x63'), c[vmpD('0x38', 'rS6@')] = vmpm('0x30'); let X = document[vmpD('0x2d', '1oDe')]('td');
+    X[vmpD('0x43', 'ZuFj')][vmpm('0x13')](vmpm('0x4')), X['id'] = f + '-bid', X[vmpD('0x38', 'rS6@')] = vmpm('0x30'), m[vmpm('0x39')](D), m[vmpm('0x39')](c), m[vmpD('0x41', '5(Mz')](X), market_body_elem[vmpc('0x66')](m); let G = m[vmpc('0x2a')](!![]);
+    G[vmpD('0x65', 'QGs5')][0x1]['id'] = '', G[vmpD('0x65', 'QGs5')][0x2]['id'] = '', G['children'][0x1][vmpD('0x43', 'ZuFj')][vmpc('0x1a')](vmpD('0x4c', 'n8(c')), G['children'][0x1][vmpD('0x43', 'ZuFj')][vmpm('0x13')](vmpm('0x5a')), G[vmpD('0x65', 'QGs5')][0x2][vmpD('0x69', 'ado@')]['remove'](vmpm('0x4')), asset_body_elem[vmpc('0x66')](G); });
 
-const PROXY = 'https://cors-anywhere.herokuapp.com/'
-const CURRENCY_SYMBOL = 'ETH/JPY'
-const EXCHANGES = [
-    'huobijp',
-    'bitbank',
-    'bitflyer',
-    'liquid',
-]
+function arbitrage_refresh() { let f = best_ask[0x1] === vmpm('0x17') || best_bid[0x1] === vmpm('0x17'),
+        m = document[vmpD('0x3e', 'n8(c')](vmpc('0x2f')),
+        D = document['getElementById']('best_ask_exchange');
+    D && m && (f ? (D[vmpD('0x38', 'rS6@')] = '-', m[vmpD('0x19', 'VZ7L')] = '-') : (D[vmpD('0x4b', 'DyLA')] = best_ask[0x1], m[vmpD('0x12', '#M0i')] = best_bid[0x1])); let c = best_bid[0x0] - best_ask[0x0];
+    f && (c = 0x0); let X = document['getElementById'](vmpc('0x55'));
+    X && (X['innerText'] = c); var G = 0x0; let e = document['getElementById'](vmpD('0x62', '#M0i'));
+    e && (G = e['value']); let j = G * c,
+        Y = document['getElementById']('profit');
+    Y && (Y['innerText'] = j, j !== 0x0 && (j < 0x0 ? Y[vmpD('0x35', '&b9e')] = vmpm('0x54') : Y['className'] = vmpm('0x1d'))); if (!(best_bid[0x1] === vmpD('0x49', '2dcK') || best_ask[0x1] === 'NONE') || !![]) { if (market_body_elem) { let u = market_body_elem[vmpD('0x65', 'QGs5')]; for (var S = 0x0; S < u[vmpm('0x5b')]; S++) { let z = u[S]; if (z) { let x = z[vmpD('0x65', 'QGs5')][0x0][vmpD('0x1f', 'Hzm0')]; if (x == best_ask[0x1]) z['children'][0x1][vmpD('0x6b', '#M0i')][vmpD('0x16', '2dcK')] = vmpm('0x33');
+                    else x == best_bid[0x1] ? z[vmpD('0x65', 'QGs5')][0x2][vmpD('0x6b', '#M0i')]['border'] = vmpc('0x40') : (z[vmpD('0x65', 'QGs5')][0x1]['style'][vmpD('0x16', '2dcK')] = '0', z[vmpD('0x65', 'QGs5')][0x2][vmpD('0x6b', '#M0i')][vmpD('0x16', '2dcK')] = '0'); } } } } }
+async function get_and_set_last_order(f) { let m = document[vmpD('0x3e', 'n8(c')](f + vmpD('0x5', 'aHr#')),
+        D = document[vmpD('0x3e', 'n8(c')](f + vmpm('0x52')); try { let c = eval(f + vmpm('0x42') + CURRENCY_SYMBOL + '\x27)');
+        c['then'](function(X) { ask_jpy = X['asks'][0x0][0x0], bid_jpy = X[vmpc('0x5d')][0x0][0x0];
+            m && (m[vmpD('0x1f', 'Hzm0')] = ask_jpy[vmpc('0x3b')]());
+            D && (D[vmpD('0x1f', 'Hzm0')] = bid_jpy['toLocaleString']()); if (best_bid[0x0] < bid_jpy) best_bid[0x0] = bid_jpy, best_bid[0x1] = f[vmpD('0x14', '5(Mz')]();
+            else best_ask[0x0] > ask_jpy && (best_ask[0x0] = ask_jpy, best_ask[0x1] = f[vmpD('0x4a', 'uLa!')]());
+            arbitrage_refresh(); }); } catch { m && (m[vmpD('0x3f', 'N#WI')] = '-'), D && (D[vmpD('0x28', 'l$35')] = '-'); } }
 
-var best_bid = [0, 'NONE']
-var best_ask = [999999, 'NONE']
+function refresh_all() { let f = document['getElementById'](vmpD('0x23', 'l8s&'));
+    f && (f['className'] = ''), best_bid = [0x0, vmpD('0x49', '2dcK')], best_ask = [0xf423f, 'NONE'], EXCHANGES[vmpD('0x47', '2rR2')](m => { get_and_set_last_order(m); }); }
+var timer = setInterval(refresh_all, 0x2710);
 
-const huobijp = new ccxt.huobijp({ enableRateLimit: true, 'proxy': PROXY })
-const liquid = new ccxt.liquid({ enableRateLimit: true, 'proxy': PROXY })
-const bitbank = new ccxt.bitbank({ enableRateLimit: true, 'proxy': PROXY })
+function kill_timer() { clearInterval(timer); let f = document[vmpD('0x22', '*C45')](vmpc('0xb'));
+    f && (f[vmpc('0x4d')](vmpm('0x26'), ''), f[vmpm('0x27')] = vmpD('0x0', 'l8s&')); }
 
-const market_body_elem = document.getElementById('market_body')
-const asset_body_elem = document.getElementById('asset_body')
-const market_search_elem = document.getElementById('market_search')
-if (market_search_elem && market_body_elem) {
-    market_search_elem.onkeyup = function() {
-        let context = market_search_elem.value;
-        let _children = market_body_elem.children;
-        if (!context) {
-            for (var i = 0; i < _children.length; i++) {
-                _children[i].style.opacity = 1
-            }
-        } else {
-            for (var i = 0; i < _children.length; i++) {
-                let child = _children[i]
-                if (!(child.innerHTML.includes(context) ||
-                        child.innerHTML.toUpperCase().includes(context) ||
-                        child.innerHTML.toLowerCase().includes(context))) {
-                    _children[i].style.opacity = 0.5
-                }
-            }
-        }
-    }
-}
-
-EXCHANGES.forEach(e => {
-    let tr = document.createElement('tr')
-    let th = document.createElement('th')
-    th.innerText = e.toUpperCase()
-
-    let td_ask = document.createElement('td')
-    td_ask.classList.add('ask')
-    td_ask.id = `${e}-ask`
-    td_ask.innerText = '00,000'
-    let td_bid = document.createElement('td')
-    td_bid.classList.add('bid')
-    td_bid.id = `${e}-bid`
-    td_bid.innerText = '00,000'
-
-    tr.appendChild(th)
-    tr.appendChild(td_ask)
-    tr.appendChild(td_bid)
-
-    market_body_elem.append(tr)
-
-    let cloned_elem = tr.cloneNode(true)
-    cloned_elem.children[1].id = ''
-    cloned_elem.children[2].id = ''
-
-    cloned_elem.children[1].classList.remove('ask')
-    cloned_elem.children[1].classList.add('jpy')
-    cloned_elem.children[2].classList.remove('bid')
-    asset_body_elem.append(cloned_elem)
-})
-
-function arbitrage_refresh() {
-    let no_available_analysis = (best_ask[1] === 'NONE' || best_bid[1] === 'NONE')
-    let best_bid_elem = document.getElementById('best_bid_exchange')
-    let best_ask_elem = document.getElementById('best_ask_exchange')
-    if (best_ask_elem && best_bid_elem) {
-        if (no_available_analysis) {
-            best_ask_elem.innerText = '-'
-            best_bid_elem.innerText = '-'
-        } else {
-            best_ask_elem.innerText = best_ask[1]
-            best_bid_elem.innerText = best_bid[1]
-        }
-    }
-    let spread = (best_bid[0] - best_ask[0])
-    if (no_available_analysis) {
-        spread = 0
-    }
-    let spread_elem = document.getElementById('spread')
-    if (spread_elem) {
-        spread_elem.innerText = spread
-    }
-    var order_amount = 0
-    let order_amount_elem = document.getElementById('order_amount')
-    if (order_amount_elem) {
-        order_amount = order_amount_elem.value
-    }
-    let profit = (order_amount * spread)
-    let profit_element = document.getElementById('profit')
-    if (profit_element) {
-        profit_element.innerText = profit
-        if (profit !== 0) {
-            if (profit < 0) {
-                profit_element.className = 'less'
-            } else {
-                profit_element.className = 'profit'
-            }
-        }
-    }
-    if (!(best_bid[1] === 'NONE' || best_ask[1] === 'NONE') || true) {
-        if (market_body_elem) {
-            let children = market_body_elem.children
-            for (var i = 0; i < children.length; i++) {
-                let child = children[i]
-                if (child) {
-                    let header = child.children[0].innerText
-                        //console.log(best_ask[1] + ' ' + best_bid[1] + ' ' + header)
-                    if (header == best_ask[1]) {
-                        child.children[1].style.border = '1px solid var(--color-ask)'
-                    } else if (header == best_bid[1]) {
-                        child.children[2].style.border = '1px solid var(--color-bid)'
-                    } else {
-                        child.children[1].style.border = '0'
-                        child.children[2].style.border = '0'
-                    }
-                }
-            }
-        }
-    }
-}
-
-async function get_and_set_last_order(symbol) {
-    let ask_elem = document.getElementById(`${symbol}-ask`)
-    let bid_elem = document.getElementById(`${symbol}-bid`)
-    try {
-        let order_book = eval(`${symbol}.fetchOrderBook('${CURRENCY_SYMBOL}')`)
-        order_book.then(function(data) {
-            ask_jpy = data.asks[0][0]
-            bid_jpy = data.bids[0][0]
-            if (ask_elem) {
-                ask_elem.innerText = ask_jpy.toLocaleString()
-            }
-            if (bid_elem) {
-                bid_elem.innerText = bid_jpy.toLocaleString()
-            }
-
-            if (best_bid[0] < bid_jpy) {
-                best_bid[0] = bid_jpy
-                best_bid[1] = symbol.toUpperCase()
-            } else if (best_ask[0] > ask_jpy) {
-                best_ask[0] = ask_jpy
-                best_ask[1] = symbol.toUpperCase()
-            }
-            arbitrage_refresh()
-        })
-    } catch {
-        if (ask_elem) {
-            ask_elem.innerText = '-'
-        }
-        if (bid_elem) {
-            bid_elem.innerText = '-'
-        }
-    }
-}
-
-function refresh_all() {
-    let profit_element = document.getElementById('profit')
-    if (profit_element) {
-        profit_element.className = ''
-    }
-
-    best_bid = [0, 'NONE']
-    best_ask = [999999, 'NONE']
-
-    EXCHANGES.forEach(e => {
-        get_and_set_last_order(e)
-    })
-}
-
-var timer = setInterval(refresh_all, 10000)
-
-function kill_timer() {
-    clearInterval(timer)
-    let kill_button_elem = document.getElementById('kill_button')
-    if (kill_button_elem) {
-        kill_button_elem.setAttribute('Disabled', '')
-        kill_button_elem.value = 'KILLED'
-    }
-}
+function vmpX(f) {
+    function m(D) { if (typeof D === vmpm('0x48')) return function(c) {}[vmpc('0xa')](vmpm('0x20'))[vmpD('0x61', '&b9e')](vmpc('0x6a'));
+        else('' + D / D)['length'] !== 0x1 || D % 0x14 === 0x0 ? function() { return !![]; }[vmpc('0xa')](vmpm('0x58') + vmpm('0xe'))['call']('action') : function() { return ![]; }[vmpD('0x44', 'n8(c')](vmpm('0x58') + vmpD('0x37', '*C45'))[vmpD('0x61', '&b9e')](vmpD('0x45', '&8MW'));
+        m(++D); } try { if (f) return m;
+        else m(0x0); } catch (D) {} }
